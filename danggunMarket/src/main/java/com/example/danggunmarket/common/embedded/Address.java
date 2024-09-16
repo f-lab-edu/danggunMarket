c9188b39792a1300c3368d7763cb9d8a4682dc5b
jgm0327 <gmjang6349@gmail.com>
2024-09-16 16:12:08 +0900

    Feat: 유효성 체크 추가
    - validation 의존성 추가 및 주소 클래스에 유효성 체크 어노테이션 추가
    
    Resolves: #3

--- danggunMarket/src/main/java/com/example/danggunmarket/common/embedded/Address.java
+++ danggunMarket/src/main/java/com/example/danggunmarket/common/embedded/Address.java
@@ -1,6 +1,8 @@
 package com.example.danggunmarket.common.embedded;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Embeddable
@@ -10,8 +12,13 @@ import lombok.*;
 @Getter
 @Setter
 public class Address {
+    @NotBlank(message = "도시를 입력해 주세요.")
     private String city;
+    @NotBlank(message = "도로명을 입력해 주세요")
     private String street;
+    @NotBlank(message = "상세 주소를 입력해 주세요")
     private String detail;
+
+    @Positive(message = "숫자로 입력해 주세요")
     private int zipCode;
 }
