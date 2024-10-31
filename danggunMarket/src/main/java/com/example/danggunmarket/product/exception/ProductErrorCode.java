cd493fda10ac12dc707b201eb5ec2aedab323c8d
jgm0327 <gmjang6349@gmail.com>
2024-10-31 16:55:20 +0900

    Fix: ProductErrorCode 수정
    - PRODUCT가 아닌 MEMBER로 작성한 거 수정
    
    fix: #9

--- danggunMarket/src/main/java/com/example/danggunmarket/product/exception/ProductErrorCode.java
+++ danggunMarket/src/main/java/com/example/danggunmarket/product/exception/ProductErrorCode.java
@@ -5,7 +5,7 @@ import com.example.danggunmarket.common.exception.ErrorCode;
 
 public enum ProductErrorCode implements ErrorCode {
 
-    NOT_FOUND_MEMBER(404, "존재하지 않는 상품입니다."),
+    NOT_FOUND_PRODUCT(404, "존재하지 않는 상품입니다."),
     NOT_VALID_ID(400, "유효하지 않은 상품 ID입니다"),
     NOT_AUTHORIZED_PRODUCT(403, "사용자는 상품 판매자가 아닙니다");
 
